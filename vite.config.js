8cc6384f1fe8a9c548e7049041a3cb0232c60f8e
Kelvin Ho <kelvinho159@gmail.com>
2025-07-19 09:03:39 +0000

    chore: allow host all for interview

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ["d47vlt-5173.csb.app"],
+    allowedHosts: true,
   },
   plugins: [react()],
 });
