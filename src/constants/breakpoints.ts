9f15f9e24012f2a6c2d278d1781c53e83270d417
Kelvin Ho <kelvinho159@gmail.com>
2025-07-25 01:53:46 +0000

    feat: add breakpoints

--- src/constants/breakpoints.ts
+++ src/constants/breakpoints.ts
@@ -1,5 +1,5 @@
 export const BREAKPOINTS = {
-  // tabletMax: 1024,
+  tabletMax: 1200,
   mobileMax: 1024,
   // mobileMax: 767,
 };
--- src/content/app/description/DescriptionItem.tsx
+++ src/content/app/description/DescriptionItem.tsx
@@ -12,7 +12,7 @@ const DescriptionItem: React.FC<DescriptionItemProps> = ({
   return (
     <div className="flex gap-[40px]">
         <h3 className="
-          relative text-[34px] font-[700] leading-[120%] h-fit
+          relative text-[34px] font-[700] leading-[120%] h-fit w-min
           after:absolute after:content-[''] after:block after:w-[25px] after:h-[25px] after:border-[8px] after:border-[#AA666699] after:rounded-full after:bg-transparent after:bottom-[-15px] after:right-[-15px]
         ">
         {title}
